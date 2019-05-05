462325ca77b8c9b8cb8abdbcc3bde21295a5ff5a
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2019-05-05 14:37:17 +0300

    str file changed

--- python/str_ile_calismak.py
+++ python/str_ile_calismak.py
@@ -31,8 +31,8 @@ print(dir(first_name))  # Tum metodlara burdan bakabiliriz
 
 print(len(first_name + last_name))
 
-content = "Lorem Ipsum Dolor" \
 "dis amet :)" \
+content = "Lorem Ipsum Dolor" \
 "last line" 
 
 
@@ -76,3 +76,30 @@ print(age_info)
 age_str = str(age)
 
 print(age_str)
+
+age_with_format = "Merhaba {} {}, senin \
+yasin gelecek sene {} olacak".format(
+    first_name, last_name, age + 1
+)
+
+age_with_format_2 = "Merhaba {frst} {lst}, senin \
+yasin gelecek sene {age} olacak".format(
+    age=age+1, frst=first_name, lst=last_name
+)
+print(age_with_format_2)
+
+
+age_with_f = f"Merhaba {first_name[0]}. {last_name}, senin yasin \
+gelecek sene {age + 1} olacak "
+print(age_with_f)
+
+
+print(f"{first_name[0]}. {last_name}")
+print(first_name[0] + ".", last_name)
+print(first_name[:2] + ".", last_name)
+print(first_name[:100] + ".", last_name)
+print(first_name[0:5:2] + ".", last_name)
+print(first_name[::-1] + ".", last_name)
+
+
+print(f"{last_name[0:2]}{last_name[-1]}")
